5b0e3f990a281626e291d48db806148ad719b11a
ryfazrin <riansera515@gmail.com>
2023-02-09 11:23:36 +0700

    show html

--- app.js
+++ app.js
@@ -2,13 +2,18 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+var path = require('path');
+var public = __dirname + '/public/';
+
 app.get('/', (req, res) => {
   res.set({
     'Cross-Origin-Embedder-Policy': 'credentialless',
     'Cross-Origin-Opener-Policy': 'same-origin'
   })
-  res.send('Hello World!')
+  res.sendFile(path.join(public + 'index.html'))
 })
+
+app.use('/', express.static(public))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
